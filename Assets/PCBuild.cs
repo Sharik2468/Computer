b30a86ef39b9affc75c4f4a1c179a07d411e1726
Sharik2468 <nsharabanov@bk.ru>
2022-04-30 18:52:22 +0300

    30.04

--- Assets/PCBuild.cs
+++ Assets/PCBuild.cs
@@ -14,6 +14,7 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
     int PE = 0;
     public Button CoolerBtn;
     public GameObject Cooler;
+    public GameObject Cooler1;
     int CE = 0;
     public Button PUBtn;
     public GameObject PU;
@@ -26,7 +27,22 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
     public Button MBBtn;
     public GameObject MB;
     int MBE = 0;//чётные - вставлено
+    public Button KrBtn;
+    public GameObject Kr;
+    int KrE = 0;
+    public Button PowerButtonButton;
+    public GameObject PowerButton;
+    int PBE = 0;
 
+    public GameObject MonitorScreen;
+    public Material[] ScreenMaterials;
+
+    MeshRenderer MeshRender;
+    float timeLeft = -1.0f;
+
+    int l;
+
+    AudioSource m_MyAudioSource;
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +54,8 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
         RAMBtn.onClick.AddListener(RAMFunc);
         RAMBtn.onClick.AddListener(RAM1Func);
         MBBtn.onClick.AddListener(MBFunc);
+        KrBtn.onClick.AddListener(KrFunc);
+        PowerButtonButton.onClick.AddListener(PowerButtonClick);
         PU.GetComponent<Button>().onClick.AddListener(PUFunc);
         Cooler.GetComponent<Button>().onClick.AddListener(CoolerFunc);
         VideoCard.GetComponent<Button>().onClick.AddListener(VideoCardInFunc);
@@ -45,13 +63,28 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
         RAM1.GetComponent<Button>().onClick.AddListener(RAMFunc);
         RAM2.GetComponent<Button>().onClick.AddListener(RAM1Func);
         MB.GetComponent<Button>().onClick.AddListener(MBFunc);
+        Kr.GetComponent<Button>().onClick.AddListener(KrFunc);
+        PowerButton.GetComponent<Button>().onClick.AddListener(PowerButtonClick);
 
-        ProcessorBtn.enabled = false;
+        MeshRender = MonitorScreen.GetComponent<MeshRenderer>();
+
+        m_MyAudioSource = GetComponent<AudioSource>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
+
+        if (timeLeft >= 0)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                PCChangeMaterial();
+            }
+        }
+
         if (CE % 2 == 0)
             ProcessorBtn.enabled = false;
         else
@@ -67,7 +100,18 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
         else
             MBBtn.enabled = true;
 
-        if(MBE%2==0)
+        if (VCE % 2 == 0 && //
+            PE % 2 == 0 && //
+            CE % 2 == 0 &&//
+            PUE % 2 == 0 &&//
+            RAM1E % 2 == 0 &&//
+            RAM2E % 2 == 0 &&
+            MBE % 2 == 0)
+            KrBtn.enabled = true;
+        else
+            KrBtn.enabled = false;
+
+        if (MBE % 2 == 0)
         {
             VideoCardBtn.enabled = true;
             ProcessorBtn.enabled = true;
@@ -75,7 +119,7 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
             PUBtn.enabled = true;
             RAMBtn.enabled = true;
         }
-        else 
+        else
         {
             VideoCardBtn.enabled = false;
             ProcessorBtn.enabled = false;
@@ -83,11 +127,41 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
             PUBtn.enabled = false;
             RAMBtn.enabled = false;
         }
+
+        if (KrE % 2 == 0)
+        {
+            VideoCardBtn.enabled = false;
+            ProcessorBtn.enabled = false;
+            CoolerBtn.enabled = false;
+            PUBtn.enabled = false;
+            RAMBtn.enabled = false;
+            MBBtn.enabled = false;
+        }
+        else
+        {
+            VideoCardBtn.enabled = true;
+            ProcessorBtn.enabled = true;
+            CoolerBtn.enabled = true;
+            PUBtn.enabled = true;
+            RAMBtn.enabled = true;
+            MBBtn.enabled = true;
+        }
+
+        if (VCE % 2 == 0 && //
+            PE % 2 == 0 && //
+            CE % 2 == 0 &&//
+            PUE % 2 == 0 &&//
+            RAM1E % 2 == 0 &&//
+            RAM2E % 2 == 0 &&
+            MBE % 2 == 0)
+            PowerButtonButton.enabled = true;
+        else
+            PowerButtonButton.enabled = false;
     }
 
     void VideoCardInFunc()
     {
-        if (MBE % 2 == 0)
+        if (MBE % 2 == 0 && KrE % 2 != 0 && PBE==0)
         {
             Animator m_Animator = VideoCard.GetComponent<Animator>();
             Debug.Log("You have clicked the button!");
@@ -98,7 +172,7 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
 
     void ProcessorFunc()
     {
-        if (CE % 2 != 0 && MBE%2==0)
+        if (CE % 2 != 0 && MBE % 2 == 0 && KrE % 2 != 0 && PBE == 0)
         {
             Animator m_Animator = Processor.GetComponent<Animator>();
             Debug.Log("You have clicked the button!");
@@ -109,7 +183,7 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
 
     void CoolerFunc()
     {
-        if (MBE % 2 == 0)
+        if (MBE % 2 == 0 && KrE % 2 != 0 && PBE == 0)
         {
             Animator m_Animator = Cooler.GetComponent<Animator>();
             Debug.Log("You have clicked the button!");
@@ -120,7 +194,7 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
 
     void PUFunc()
     {
-        if (MBE % 2 == 0)
+        if (MBE % 2 == 0 && KrE % 2 != 0 && PBE == 0)
         {
             Animator m_Animator = PU.GetComponent<Animator>();
             Debug.Log("You have clicked the button!");
@@ -130,7 +204,7 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
     }
     void RAMFunc()
     {
-        if (MBE % 2 == 0)
+        if (MBE % 2 == 0 && KrE % 2 != 0 && PBE == 0)
         {
             Animator m_Animator = RAM1.GetComponent<Animator>();
             Debug.Log("You have clicked the button!");
@@ -141,7 +215,7 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
 
     void RAM1Func()
     {
-        if (MBE % 2 == 0)
+        if (MBE % 2 == 0 && KrE % 2 != 0 && PBE == 0)
         {
             Animator m_Animator = RAM2.GetComponent<Animator>();
             Debug.Log("You have clicked the button!");
@@ -157,7 +231,8 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
             CE % 2 != 0 &&//
             PUE % 2 != 0 &&//
             RAM1E % 2 != 0 &&//
-            RAM2E % 2 != 0)
+            RAM2E % 2 != 0 &&
+            KrE % 2 != 0 && PBE == 0)
         {
             Animator m_Animator = MB.GetComponent<Animator>();
             Debug.Log("You have clicked the button!");
@@ -166,8 +241,69 @@ public class PCBuild : MonoBehaviour, IPointerClickHandler
         }
     }
 
+    void KrFunc()
+    {
+        if (VCE % 2 == 0 && //
+            PE % 2 == 0 && //
+            CE % 2 == 0 &&//
+            PUE % 2 == 0 &&//
+            RAM1E % 2 == 0 &&//
+            RAM2E % 2 == 0 &&
+            MBE % 2 == 0)
+        {
+            Animator m_Animator = Kr.GetComponent<Animator>();
+            Debug.Log("You have clicked the button!");
+            m_Animator.SetTrigger("ButtonClick");
+            KrE++;
+        }
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Click Received");
     }
+
+    void PowerButtonClick()
+    {
+        if (VCE % 2 == 0 && //
+            PE % 2 == 0 && //
+            CE % 2 == 0 &&//
+            PUE % 2 == 0 &&//
+            RAM1E % 2 == 0 &&//
+            RAM2E % 2 == 0 &&
+            MBE % 2 == 0)
+        {
+            Animator m_Animator = Cooler1.GetComponent<Animator>();
+            Debug.Log("PC Power Button Clicked");
+            m_Animator.SetTrigger("PowerButtonClick");
+            timeLeft = 3.0f;
+            m_MyAudioSource.Play();
+            //PBE++;
+        }
+    }
+
+    private void PCChangeMaterial()
+    {
+        if (PBE % 2 != 0)
+        {
+            if (PBE == 3)
+            {
+                PBE = 0;
+                MeshRender.material = ScreenMaterials[0];
+                m_MyAudioSource.Stop();
+                return;
+            }
+            PBE++;
+            MeshRender.material = ScreenMaterials[PBE];
+            //MBE++;
+        }
+        else
+        {
+            PBE++;
+            MeshRender.material = ScreenMaterials[PBE];
+            //MBE++;
+            timeLeft = 3.0f;
+        }
+    }
+
 }
